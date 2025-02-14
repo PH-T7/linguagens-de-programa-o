3b4dfebd53ba024adaf5457578097976eabed1f7
Aluno <aluno@facens.br>
2025-02-13 22:24:05 -0300

    first commit

---
+++ Aula_1/licao.js
@@ -0,0 +1,40 @@
+/* Lição 1 
+let num = 1 ;
+let num2 = 3;
+
+
+console.log (num + num2 )
+
+console.log (num - num2 )
+
+console.log (num * num2 )
+
+console.log (num / num2 )
+
+*/
+
+// lição 2
+/*
+
+let num1 = [100];
+
+console.log()
+*/
+
+// lição 3
+/*
+
+let nome = 'Disciplina de Programação para web'
+
+console.log(nome.length);
+*/
+//lição 4
+
+var nome = "Fabio Rodrigo Comlobini"
+
+
+re = nome.substring(5, 13);
+var separador = nome.split(re);
+
+
+console.log (separador);
