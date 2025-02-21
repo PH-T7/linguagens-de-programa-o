9a48512e52fec4245564f8a362ec79af9a4fb781
Aluno <aluno@facens.br>
2025-02-20 22:23:28 -0300

    20/02/2025

--- Aula_2/licao2.js
+++ Aula_2/licao2.js
@@ -13,8 +13,32 @@ let calculaAreaRetangulo = (a, b) => a * b;
 console.log(calculaAreaRetangulo(2,5));
 */
 //Lição 3
+/*
 let array = [10, 20, 30, 40, 50];
 
 function somar(soma){
 let  soma = array[0]+[1]+[2]+[3]+[4]
 }
+*/
+//Lição 4
+//...
+//Lição 5
+/*
+let cidades = ["Sorocaba","Piedade","Votorantim"];
+
+cidades.unshift("São paulo");
+cidades.pop();
+
+console.log(cidades);
+*/
+//lição 6
+const aluno ={
+    nome: 'Fabio',
+    idade: 20,
+    curso: 'Engenharia',
+    matricula: 'Confirmado'
+
+
+}
+
+console.log(aluno.nome,"/",aluno.idade,"/",aluno.curso,"/",aluno.matricula);
