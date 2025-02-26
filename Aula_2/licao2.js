413feb496e37da8415b8bcaa43df801f880f25a7
PH <140414857+PH-T7@users.noreply.github.com>
2025-02-26 15:38:03 -0300

    Update licao2.js

--- Aula_2/licao2.js
+++ Aula_2/licao2.js
@@ -1,4 +1,4 @@
-*
+/*
 //Lição 1
 let recebeNome = function(a, b){
     return a + " " + b ;
@@ -67,3 +67,74 @@ const aluno ={
 console.log(aluno.nome,"/",aluno.idade,"/",aluno.curso,"/",aluno.matricula);
 */
 //licao 7
+/*
+let array = [1,2,3,4,5,6,7,8,9,10];
+
+
+let encontrarMaior = function(num){
+    let maior = num[0];
+
+    for (let i = 1; i < num.length; i++) {
+        if (num[i] > maior){
+            maior = num[i];
+        }
+    }
+    return maior;
+}
+
+
+console.log(encontrarMaior(array));
+
+*/
+//licao 8
+/*
+const produto ={
+    "nome": "Leite",
+    "preco": 5,
+    "quantidade": 2,
+
+    cacularTotal : function(){
+
+    return this.preco * this.quantidade;
+    }
+
+};
+
+
+console.log(produto.cacularTotal())
+
+*/
+//licao 9
+/*
+const endereco = {
+    rua: "Avenida Brasil",
+    numero: 123,
+    bairro: "Centro",
+    cidade: "São Paulo"
+};
+
+
+for (let chave in endereco) {
+    console.log(chave + ": " + endereco[chave]);
+}
+*/
+//Licao 10
+const alunos = [
+    { nome: "Ana", idade: 17 },
+    { nome: "Bruno", idade: 19 },
+    { nome: "Carlos", idade: 21 },
+    { nome: "Daniela", idade: 16 }
+];
+
+function alunosMaior(lista) {
+    let maiores = []; 
+
+    for (let i = 0; i < lista.length; i++) { 
+        if (lista[i].idade > 18) { 
+            maiores.push(lista[i]); 
+        }
+    }
+    return maiores;
+}
+
+console.log(alunosMaior(alunos));
