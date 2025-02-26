192d491d85467e1a5a62718456a073ad2c97d478
PH <140414857+PH-T7@users.noreply.github.com>
2025-02-25 22:45:11 -0300

    Update licao2.js

--- Aula_2/licao2.js
+++ Aula_2/licao2.js
@@ -1,10 +1,10 @@
-/*
+*
 //Lição 1
 let recebeNome = function(a, b){
-    return a = "Fabio", b = "Comlobini";
+    return a + " " + b ;
 };
 
-console.log(recebeNome)
+console.log(recebeNome("Fabio","Colombini"))
 */
 //Lição 2
 /*
@@ -16,12 +16,34 @@ console.log(calculaAreaRetangulo(2,5));
 /*
 let array = [10, 20, 30, 40, 50];
 
-function somar(soma){
-let  soma = array[0]+[1]+[2]+[3]+[4]
+let somaArray= function(arr){
+    let soma = 0; 
+    for (let i = 0; i < arr.length; i++) { 
+        soma += arr[i]; 
+    }
+    return soma;
 }
+
+
+console.log(somaArray(array));
 */
 //Lição 4
-//...
+/*
+let array = [1,2,3,4,5,6,7,8,9,10];
+
+
+let imparArray = function(imp){
+    let impar = []
+    for (let i = 0; i < imp.length; i++) {
+        if (imp[i] % 2 !== 0){
+            impar.push(imp[i]);
+        }
+    }
+    return impar;
+}
+
+console.log(imparArray(array));
+*/
 //Lição 5
 /*
 let cidades = ["Sorocaba","Piedade","Votorantim"];
@@ -32,6 +54,7 @@ cidades.pop();
 console.log(cidades);
 */
 //lição 6
+/*
 const aluno ={
     nome: 'Fabio',
     idade: 20,
@@ -42,3 +65,5 @@ const aluno ={
 }
 
 console.log(aluno.nome,"/",aluno.idade,"/",aluno.curso,"/",aluno.matricula);
+*/
+//licao 7
